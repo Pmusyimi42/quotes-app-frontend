1378d630937c5d46e4767fa9f934e189a2503276
patrick kisyula <pmusyimi42@gmail.com>
2023-03-06 09:39:49 +0300

    set up routing and fetching

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import AddQuote from './components/AddQuote';
+import DisplayQuotes from './components/DisplayQuotes';
+import { useEffect, useState } from 'react';
+import { Routes, Route} from "react-router-dom"
+import Comments from './components/Comments';
+import AddComment from './components/AddComment';
 
 function App() {
+
+  const [quotes, setQuote] = useState([])
+  const [comments, setComment] = useState([])
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:9292/quotes")
+    .then(res=>res.json())
+    .then(data=>setQuote(data))
+  },[])
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:9292/comments")
+    .then(res=>res.json())
+    .then(data=>setComment(data))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar/>
+        <Routes>
+          <Route path='/displayQuotes' element={<DisplayQuotes quotes = {quotes} comments = {comments}/>}/>
+          <Route path='/addQuotes' element= {<AddQuote/>}/>
+          <Route path= '/home' exact element = {<Home/>}/>
+          <Route path= '/displayQuotes/comments' element= {<Comments comments={comments}/>}/>
+          <Route path= '/displayComments/comments/addComments' element= {<AddComment/>}/>
+        </Routes>
+        
     </div>
   );
 }
